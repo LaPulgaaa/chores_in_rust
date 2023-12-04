473b932510077f01ddebd0da16ddc2f904149d8e
Varun Singh <varunsingh9114@gmail.com>
2023-12-05 03:04:34 +0530

    added basic struct for the task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+use chrono::prelude::*;
 fn main() {
     println!("Hello, world!");
+    add_task();
+}
+#[derive(Parser,Debug)]
+#[command(name="to do list")]
+#[command(author,version,about)]
+struct Task{
+    #[arg(long)]
+    title:String,
+    #[arg(long)]
+    discription:String,
+    #[arg(long)]
+    deadline:DateTime<Local>
+}
+fn add_task(){
+    let field=Task::parse();
+    println!("{}",field.title);
 }
