27e5bb051d3a1bb03f11caf5fc317a90abe30560
Varun Singh <varunsingh9114@gmail.com>
2023-12-08 02:08:02 +0530

    small changes. add read and write features

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,49 @@ fn main() {
     println!("Hello, world!");
     add_task();
 }
+#[derive(Debug)]
+enum Status {
+    Complete(String),
+    Uncomplete(String)
+}
 #[derive(Parser,Debug)]
 #[command(name="to do list")]
 #[command(author,version,about)]
 struct Task{
-    ///enter title
+
+    #[arg(long,short,default_value_t =String::from("Varun Singh"))]
+    name:String,
+
+    #[arg(long,short)]
     title:String,
-    ///enter discription of the title
+
+    #[arg(long,short)]
     discription:String,
 
 }
 fn add_task(){
     let field=Task::parse();
-    println!("To do :");
-    println!("task -> {}",field.title);
-    println!("details -> {}",field.discription);
+   
+    //struct for one task
+    struct Todo{
+        name:String,
+        task:String,
+        describe:String,
+        status:Status
+    }
+
+    let add_task=Todo{
+        name:field.name,
+        task:field.title,
+        describe:field.discription,
+        status:Status::Uncomplete(String::from("Not Completed"))
+    };
+
+    println!("name:=>{}",add_task.name);
+    println!("task:=>{}",add_task.task);
+    println!("discription:=>{}",add_task.describe);
+    println!("{:?}",add_task.status)
+
+
+
 }
